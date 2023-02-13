6b838fd03e2a337f08282b72c8f7a3a9476184b3
BagaBaga <bdesign@bk.ru>
2023-02-13 13:51:37 +0300

    как-то так

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -6,19 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarService {
-    private List<Car> cars = new ArrayList<>();
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("LADA", "Ivan"));
+        cars.add(new Car("LADA", "Anton"));
+        cars.add(new Car("LADA", "Ian"));
+        cars.add(new Car("LADA", "Ivar"));
+        cars.add(new Car("LADA", "Anna"));
+    }
 
     public List<Car> getCarsToDisplay(Integer count) {
-        Car car1 = new Car("LADA", "Ivan");
-        Car car2 = new Car("LADA", "Anton");
-        Car car3 = new Car("LADA", "Ian");
-        Car car4 = new Car("LADA", "Ivar");
-        Car car5 = new Car("LADA", "Anna");
-        getCars().add(car1);
-        getCars().add(car2);
-        getCars().add(car3);
-        getCars().add(car4);
-        getCars().add(car5);
         if (count > 5|| count <= 0) {
             return getCars().stream().limit(5).toList();
         } else {
@@ -28,9 +27,5 @@ public class CarService {
 
     public List<Car> getCars() {
         return cars;
-    }
-
-    public void setCars(List<Car> cars) {
-        this.cars = cars;
     }
 }
